695eb72ab60b963e38d750303d2151bd56692021
Arjun N <arjn@deloitte.com>
2026-01-11 14:10:37 +0000

    Changed the F4 For Condition Record

--- app/initiator/webapp/controller/Submission.controller.js
+++ app/initiator/webapp/controller/Submission.controller.js
@@ -6,7 +6,7 @@ sap.ui.define([
   "sap/ui/model/Filter",
   "sap/ui/model/FilterOperator",
   "com/deloitte/mdg/salepricing/initiator/initiator/model/keyCombinations" // KeyCombination module
-  
+
 ], function (Controller, Fragment, JSONModel, MessageToast, Filter, FilterOperator, KeyCombinations) {
   "use strict";
 
@@ -125,74 +125,6 @@ sap.ui.define([
       this._dialog_createConditionRecord.close();
     },
 
-    onConditionTypeLiveChange: function (oEvent) {
-      const oInput = oEvent.getSource();
-      if (oInput.getValue()) {
-        oInput.setValueState("None");
-      }
-    },
-
-    onConditionTypeValueHelp: function (oEvent) {
-      const oView = this.getView();
-      this._oInputField = oEvent.getSource();
-      const that = this;
-
-      const fnOpenDialog = function () {
-        const oFilter = that._buildConditionTypeFilter("");
-        const oItemsBinding = that._oConditionTypeVH.getBinding("items");
-        if (oItemsBinding) {
-          oItemsBinding.filter([oFilter]);
-        }
-        that._oConditionTypeVH.open();
-      };
-
-      if (!this._oConditionTypeVH) {
-        Fragment.load({
-          id: oView.getId(),
-          name: "com.deloitte.mdg.salepricing.initiator.initiator.fragment.F4.ConditionType",
-          controller: this
-        }).then(function (oDialog) {
-          this._oConditionTypeVH = oDialog;
-          oView.addDependent(this._oConditionTypeVH);
-          fnOpenDialog();
-        }.bind(this));
-      } else {
-        fnOpenDialog();
-      }
-    },
-
-    _buildConditionTypeFilter: function (sValue) {
-      const aFilters = [new Filter("LanguageKey", FilterOperator.EQ, "EN")];
-      if (sValue) {
-        aFilters.push(new Filter([
-          new Filter("ConditionType", FilterOperator.Contains, sValue),
-          new Filter("Name", FilterOperator.Contains, sValue)
-        ], false));
-      }
-      return new Filter(aFilters, true);
-    },
-
-    onConditionTypeValueHelpSearch: function (oEvent) {
-      const sValue = oEvent.getParameter("value");
-      const oFilter = this._buildConditionTypeFilter(sValue);
-      oEvent.getSource().getBinding("items").filter([oFilter]);
-    },
-
-    onConditionTypeValueHelpConfirm: function (oEvent) {
-      const oSelectedItem = oEvent.getParameter("selectedItem");
-      if (oSelectedItem && this._oInputField) {
-        this._oInputField.setValue(oSelectedItem.getTitle());
-        this._oInputField.setValueState("None");
-      }
-      oEvent.getSource().close();
-    },
-
-    onConditionTypeSuggest: function (oEvent) {
-      const sValue = oEvent.getParameter("suggestValue");
-      const oBinding = oEvent.getSource().getBinding("suggestionItems");
-      oBinding.filter(sValue ? [this._buildConditionTypeFilter(sValue)] : [this._buildConditionTypeFilter("")]);
-    },
-
     dialog_keyCombination_select: function (oEvent) {
       const oSelectedItem = oEvent.getParameter("listItem");
       if (oSelectedItem) {
@@ -203,32 +135,32 @@ sap.ui.define([
     },
 
     dialog_keyCombination_confirm: function () {
-    const oView = this.getView();
-    const oList = Fragment.byId(oView.getId(), "KCF_Fragment_List");
-    const oSelectedItem = oList.getSelectedItem();
+      const oView = this.getView();
+      const oList = Fragment.byId(oView.getId(), "KCF_Fragment_List");
+      const oSelectedItem = oList.getSelectedItem();
 
-    if (!oSelectedItem) {
+      if (!oSelectedItem) {
         MessageToast.show("Please select a Key Combination");
         return;
-    }
+      }
 
-    // Get the key combination ID only
-    const oData = oSelectedItem.getBindingContext("kcModel").getObject();
-    this._selectedData.keyCombinationId = oData.id;
+      // Get the key combination ID only
+      const oData = oSelectedItem.getBindingContext("kcModel").getObject();
+      this._selectedData.keyCombinationId = oData.id;
 
-    // Close the dialog
-    this._dialog_keyCombination.close();
+      // Close the dialog
+      this._dialog_keyCombination.close();
 
-    // Pass both conditionType and keyCombinationId to the URL
-    this.navigateTo("RouteCreate", {
+      // Pass both conditionType and keyCombinationId to the URL
+      this.navigateTo("RouteCreate", {
         conditionType: this._selectedData.conditionType,
         keyCombinationId: this._selectedData.keyCombinationId
-    });
+      });
 
-    // Reset selection
-    this._selectedData.conditionType = "";
-    this._selectedData.keyCombinationId = "";
-},
+      // Reset selection
+      this._selectedData.conditionType = "";
+      this._selectedData.keyCombinationId = "";
+    },
 
     dialog_keyCombination_cancel: function () {
       this._dialog_keyCombination.close();
@@ -241,6 +173,128 @@ sap.ui.define([
         oInput.setValue("");
         oInput.setValueState("None");
       }
-    }
+    },
+
+    //F4 Helper Functions
+    _openF4Dialog: function (title) {
+
+      if (!this._F4Dialog) {
+        Fragment.load({
+          id: this.getView().getId(),
+          name: "com.deloitte.mdg.salepricing.initiator.initiator.fragment.F4Dialog",
+          controller: this
+        }).then((oDialog) => {
+          this._F4Dialog = oDialog;
+          this.getView().addDependent(oDialog);
+          oDialog.setTitle(title);
+          oDialog.open();
+        });
+      } else {
+        this._F4Dialog.setTitle(title);
+        this._F4Dialog.open();
+      }
+    },
+
+    onF4Search: function (oEvent) {
+      const sValue = oEvent.getParameter("newValue");
+
+      const oList = this.byId("F4List");
+      const oBinding = oList.getBinding("items");
+
+      const oFilter = new sap.ui.model.Filter({
+        filters: [
+          new sap.ui.model.Filter("title", sap.ui.model.FilterOperator.Contains, sValue),
+          new sap.ui.model.Filter("description", sap.ui.model.FilterOperator.Contains, sValue)
+        ],
+        and: false
+      });
+
+      oBinding.filter([oFilter]);
+    },
+
+    onF4Select: function (oEvent) {
+      const oItem = oEvent.getParameter("listItem");
+      if (!oItem) {
+        return;
+      }
+
+      const sValue = oItem.getTitle();
+
+      const oInput = sap.ui.getCore().byId(this._currentInputId);
+      if (oInput) {
+        oInput.setValue(sValue);
+
+        const oBinding = oInput.getBinding("value");
+        if (oBinding) {
+          oBinding.getModel().setProperty(oBinding.getPath(), sValue);
+        }
+      }
+
+      const oSearch = this.byId("F4SearchField");
+      const oList = this.byId("F4List");
+
+      if (oSearch) {
+        oSearch.setValue("");
+      }
+      if (oList) {
+        oList.getBinding("items").filter([]);
+      }
+
+      this._F4Dialog.close();
+    },
+
+
+    onF4Cancel: function () {
+      this._F4Dialog.close();
+    },
+
+    //F4 Main Functions For Field
+    onF4_Field_ConditionType: function (oEvent) {
+      sap.ui.core.BusyIndicator.show();
+
+      this._currentInputId = oEvent.getSource().getId();
+
+      const oModel = this.getOwnerComponent().getModel("SAPSalesModel");
+      const entitySet = "/ZI_CONDITIONTYPETEXT";
+
+      oModel.read(entitySet, {
+        success: (oData) => {
+          const aEN = oData.results.filter(r =>
+            r.LanguageKey === "EN" &&
+            r.ConditionType
+          );
+
+          const oSeen = {};
+          const aUnique = [];
+
+          aEN.forEach(r => {
+            if (!oSeen[r.ConditionType]) {
+              oSeen[r.ConditionType] = true;
+              aUnique.push({
+                title: r.ConditionType,
+                description: r.Name
+              });
+            }
+          });
+
+          const oF4Model = new sap.ui.model.json.JSONModel({
+            results: aUnique
+          });
+
+          this.getView().setModel(oF4Model, "F4Model");
+          this._openF4Dialog("Condition Type");
+
+          sap.ui.core.BusyIndicator.hide();
+        },
+        error: (err) => {
+          sap.ui.core.BusyIndicator.hide();
+          sap.m.MessageBox.error(
+            "Failed to load Condition Types"
+          );
+        }
+      });
+    },
+
+
   });
 });
